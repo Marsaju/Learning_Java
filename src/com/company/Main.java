f6c31ad3f2ee2c6dde66c81abe46e000f7ac14d8
Marsaju <marcelsarraseca@gmail.com>
2021-10-16 18:28:41 +0200

    Initial Committttt

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main
+{
+    int y = 10;
+
+    public static void main(String[] args)
+    {
+        Main myObj = new Main();
+
+        System.out.println(myObj.y);
+        myObj.y = 1000;
+
+        System.out.println(myObj.y);
+
+	// write your code here
+    }
+}
