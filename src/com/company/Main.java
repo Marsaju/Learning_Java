c3b2577d433e5b1cd75927387a09bde48e4a22e3
Marsaju <marcelsarraseca@gmail.com>
2021-10-16 18:48:14 +0200

    Static vs Public Methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,31 @@
 package com.company;
 
+
+// Static methods can be called without creating objects
+// Public methods must be called by creating objects
+
 public class Main
 {
     int y = 10;
+
+    public void printStatic()
+    {
+        System.out.println("Static methods can be called without creating objects");
+    }
+
+    public void printPublic()
+    {
+        System.out.println("Public methods must be called by creating objects");
+    }
 
     public static void main(String[] args)
     {
         Main myObj = new Main();
 
         System.out.println(myObj.y);
-        myObj.y = 1000;
 
-        System.out.println(myObj.y);
+        myObj.printStatic();
+        myObj.printPublic();
 
 	// write your code here
     }
