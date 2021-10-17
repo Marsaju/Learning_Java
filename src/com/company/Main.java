2fac3f2a2978b626ff177e9e10be21626a792a12
Marsaju <marcelsarraseca@gmail.com>
2021-10-17 17:17:44 +0200

    ArrayList, LinkedList, Iterator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,11 @@ import com.company.Vehicle;
 import com.company.OuterClass; // inner Class
 import com.company.Animal;
 
+import java.util.Iterator;  // Iterator for hashlist o ArrayList
+
+import java.util.LinkedList;    // LinkedList
+import java.util.ArrayList;     // Import ArrayList
+import java.util.Collections;   // Amb "sort()" pot ordenar una llista numèricament o alfabèticament
 
 // java.util package ==> date and time facilities, random-number generator and other utility classes.
 
@@ -129,5 +134,67 @@ public class Main
         }
 
 
+        // Input User
+        /*
+
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+
+            // String input
+            String name = myObj.nextLine();
+
+            // Numerical input
+            int age = myObj.nextInt();
+            double salary = myObj.nextDouble();
+
+        */
+
+
+        // Array List
+        /*
+            cars.add("something");
+            cars.remove(pos);
+            cars.set(pos, "thing");
+            cars.clear
+            cars.size();
+
+        */
+
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("Ferrari");
+        cars.add("Tesla");
+
+        Collections.sort(cars); // ordre alfabètic
+
+        for(String i: cars)
+        {
+            System.out.println(i);
+        }
+
+        // LinkedList
+        /*
+            addFirst()
+            addLast()
+            removeFirst()
+            removeLast()
+            getFirst()
+            getLast()
+        */
+        /*
+            LinkedList<String> theCar = new LinkedList<String>();
+
+            theCar.add("Laferrari");
+            theCar.add("Lamborginini");
+
+            System.out.println(theCar.getFirst());
+            System.out.println(theCar.getLast());
+
+            // Iterator
+
+                theCar.addFirst("Toyota");
+                Iterator<String> it = theCar.iterator();
+                System.out.println(it.next());
+        */
     }
+
 }
